334a029a1de25c0495a623c58d366973607d6441
Ruán <ruan.murgatroyd@sap.com>
2024-07-27 15:52:52 +0100

    [T] attempting to find stuff

--- tailwind/index.js
+++ tailwind/index.js
@@ -8,12 +8,12 @@ module.exports = {
       {
         dark: {
           ...themes.dark,
-          primary: '#83D883',
-          secondary: '#84B1C2',
-          accent: '#C185BD',
-          red: "#FF3633",
-          orange: "#FEA120",
-          yellow: "#FCFC73",
+          primary: '#880000',
+          secondary: '#880000',
+          accent: '#880000',
+          red: "#880000",
+          orange: "#880000",
+          yellow: "#880000",
           ".logo": "url(https://raw.githubusercontent.com/rjmurg/legskit/main/assets/logos/white/logo.png)"
         },
       },
